944e7f90cb14bd2a10d980d73d8a333ded9b7634
INDRA SENA REDDY C <126607980+harisreddy10@users.noreply.github.com>
2023-08-07 00:15:20 +0530

    Added solution - LeetHub

---
+++ Next Permutation - GFG/next-permutation.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int N = Integer.parseInt(in.readLine());
+            String a[] = in.readLine().trim().split("\\s+");
+            int arr[] = new int[N];
+            for(int i = 0;i < N;i++)
+                arr[i] = Integer.parseInt(a[i]);
+            
+            Solution ob = new Solution();
+            List<Integer> ans = new ArrayList<Integer>();
+            ans = ob.nextPermutation(N, arr);
+            StringBuilder out = new StringBuilder();
+            for(int i = 0;i < N;i++)
+                out.append(ans.get(i) + " ");
+            System.out.println(out);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution{
+    static List<Integer> nextPermutation(int N, int arr[]){
+        List<Integer> ans = new ArrayList<>();
+        int i=arr.length-2;
+        while(i>=0&&arr[i+1]<=arr[i]){
+            i--;
+        }
+        if(i>=0){
+            int j=arr.length-1;
+            while(j>=0&&arr[j]<=arr[i])j--;
+            swap(arr,i,j);
+        }
+        reverse(arr,i+1);
+        for(int ab:arr){
+            ans.add(ab);
+        }
+        return ans;
+    }
+    public static void swap(int[] arr,int start,int end){
+        int temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+    }
+    public static void reverse(int[] arr,int start){
+        int end=arr.length-1;
+        while(start<end){
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+}
